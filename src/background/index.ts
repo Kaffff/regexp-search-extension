a12d6660c9ff2b15ec06af4dd8939656ffdaeff1
Kaffff <takahashi.prog@gmail.com>
2022-08-03 05:03:05 +0900

    Fix: 空文字の検索に対応

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,6 +1,5 @@
 chrome.commands.onCommand.addListener(async (command) => {
   chrome.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
-    console.log(tabs);
     for (const tab of tabs) {
       chrome.scripting.executeScript({
         target: { tabId: tab.id, allFrames: false },
--- src/background/script.ts
+++ src/background/script.ts
@@ -1,16 +1,27 @@
 import Mark from "mark.js";
 
 const body = document.body;
+const mark = new Mark(body);
 const input_ele = document.createElement("input");
-input_ele.style.position = "absolute";
+input_ele.className = ".regexp-search";
+input_ele.style.position = "fixed";
 input_ele.style.top = "0px";
 input_ele.style.right = "0px";
+input_ele.style.height = "20px";
+input_ele.style.color = "#dddddd";
+input_ele.style.backgroundColor = "#222222";
 input_ele.style.zIndex = Number.MAX_SAFE_INTEGER.toString();
 input_ele.onchange = (e: InputEvent) => {
+  mark.unmark({ className: "marked" });
   if (!input_ele.value || input_ele.value.trim() == "") return;
   const re = new RegExp(input_ele.value, "gi");
-  const mark = new Mark(body);
-  mark.markRegExp(re);
+  mark.markRegExp(re, { iframes: true, className: "marked" });
 };
-document.body.appendChild(input_ele);
-input_ele.focus();
+const _input_ele = document.body.getElementsByClassName(".regexp-search");
+const is_input_ele = _input_ele.length != 0;
+if (is_input_ele) {
+  (_input_ele[0] as HTMLInputElement).focus();
+} else {
+  document.body.appendChild(input_ele);
+  input_ele.focus();
+}
